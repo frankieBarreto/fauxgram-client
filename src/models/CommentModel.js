09fbd128c04a90299d798f27c2752dd5823cb781
Frankie Barreto <barretoacquire@gmail.com>
2020-10-29 13:04:27 -0700

    created CommentMode

---
+++ src/models/CommentModel.js
@@ -0,0 +1,33 @@
+const URL = "http://localhost:3001/api/v1/comments";
+
+class CommentModel {
+  static all = () => {
+    return fetch(URL).then((response) => response.json());
+  };
+
+  static show = (commentId) => {
+    return fetch(`${URL}/${commentId}`).then((response) => response.json());
+  };
+
+  static create = (commentData) => {
+    return fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(commentData),
+    }).then((response) => response.json());
+  };
+
+  static update = (commentId, commentData) => {
+    return fetch(`${URL}/${commentId}`, {
+      method: "PUT",
+      header: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(commentData),
+    }).then((response) => response.json());
+  };
+}
+
+export default CommentModel;
